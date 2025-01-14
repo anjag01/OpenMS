8eb8f27c443a236a1fa2a76efc17c6f9c71801b1
Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>
2025-01-14 15:46:50 +0100

    fix logic add test

--- src/tests/class_tests/openms/source/IMTypes_test.cpp
+++ src/tests/class_tests/openms/source/IMTypes_test.cpp
@@ -126,6 +126,15 @@ START_SECTION(static IMFormat determineIMFormat(const MSExperiment& exp))
   }
 
   {
+    // test invalid format validation
+    MSExperiment exp;
+    MSSpectrum spec;
+    spec.setIMType(IMFormat::MIXED); // this should trigger the validation check
+    exp.addSpectrum(spec);
+    TEST_EXCEPTION(Exception::InvalidValue, IMTypes::determineIMFormat(exp))            
+  }
+
+  {
     // set both ... invalid!
     auto IMwithFDA2 = IMwithFDA;
     IMwithFDA2.setDriftTime(123.4);
