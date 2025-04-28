13711781f372b10332bd9bef6e29db8c743da2f2
Anjali Gajurel <anjag01@nero.imp.fu-berlin.de>
2025-04-28 10:53:28 +0200

    My changes for the PR

--- src/openms/source/FORMAT/DATAACCESS/sources.cmake
+++ src/openms/source/FORMAT/DATAACCESS/sources.cmake
@@ -1,8 +1,13 @@
-### the directory name
-set(directory source/FORMAT/DATAACCESS)
+# src/openms/source/FORMAT/DATAACCESS/sources.cmake
+#---------------------------------------------
+# Collect all source files in the DATAACCESS folder
+#---------------------------------------------
 
-### list all filenames of the directory here
-set(sources_list
+# Directory of this .cmeake file (holds the .cpp files)
+set(DATAACCESS_DIR ${CMAKE_CURRENT_SOURCE_DIR})
+
+# List of all .cpp files in DATAACCESS
+set(DATAACCESS_SOURCES
   MSDataWritingConsumer.cpp
   MSDataTransformingConsumer.cpp
   MSDataAggregatingConsumer.cpp
@@ -10,22 +15,18 @@ set(sources_list
   MSDataChainingConsumer.cpp
   MSDataStoringConsumer.cpp
   MSDataSqlConsumer.cpp
-  MSDataTransformingConsumer.cpp
-  MSDataWritingConsumer.cpp
   NoopMSDataConsumer.cpp
   SiriusFragmentAnnotation.cpp
   SwathFileConsumer.cpp
 )
 
-### add path to the filenames
-set(sources)
-foreach(i ${sources_list})
-	list(APPEND sources ${directory}/${i})
-endforeach(i)
+# Prepend full path to each filename
+list(TRANSFORM DATAACCESS_SOURCES PREPEND "${DATAACCESS_DIR}/")
 
-### pass source file list to the upper instance
-set(OpenMS_sources ${OpenMS_sources} ${sources})
+# Append these files into the global OpenMS_sources list
+list(APPEND OpenMS_sources ${DATAACCESS_SOURCES})
 
-### source group definition
-source_group("Source Files\\FORMAT\\DATACCESS" FILES ${sources})
+# Organize for IDEs: group these under Source Files\FORMAT\DATAACCESS
+source_group("Source Files\\FORMAT\\DATAACCESS" FILES ${DATAACCESS_SOURCES})
+
 
